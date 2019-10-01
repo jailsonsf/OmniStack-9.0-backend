739c48a93958dd975ee841aac49ef24a0b35f254
jailsonsf <jailsonsoares002@gmail.com>
2019-10-01 13:25:45 -0300

    Add connection with mongodb,  User model and  Session Controller

--- src/server.js
+++ src/server.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
 
 const app = express();
 
-app.get('/', (req, res) => {
-	return res.json({message: 'hello world'});
+require('dotenv/config');
+mongoose.connect(process.env.MONGO_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
 })
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
