c654f29f7afb346a6c6ac04aca770e0e41f5e9c1
jailsonsf <jailsonsoares002@gmail.com>
2019-10-03 16:08:32 -0300

    Update src/server.js :rocket:

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require('dotenv/config');
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -6,7 +7,6 @@ const routes = require('./routes');
 
 const app = express();
 
-require('dotenv/config');
 mongoose.connect(process.env.MONGO_URI, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
