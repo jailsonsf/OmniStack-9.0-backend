f7901224c7f86686816276c992f5944439b340c0
jailsonsf <jailsonsoares002@gmail.com>
2019-10-03 17:11:04 -0300

    Add files routes :rocket:

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ require('dotenv/config');
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 
@@ -14,6 +15,7 @@ mongoose.connect(process.env.MONGO_URI, {
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
